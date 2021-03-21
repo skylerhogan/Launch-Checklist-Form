4ee67905484fb1d91f7e8fce5d4f9ae66486f77f
Skyler Hogan <skylerhogan@skylers-air.attlocal.net>
2021-03-21 10:21:23 -0500

    completed form validation and shuttle requirements

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-    // put DOM code here to ensure elements have been loaded
     let form = document.querySelector("form");
     form.addEventListener("submit", function(event) {
-        //   let pilotName = document.querySelector("input[name=pilotName]");
-        //   let copilotName = document.querySelector("input[name=copilotName]");
-        //   let fuelLevel = document.querySelector("input[name=fuelLevel]");
-        //   let cargoMass = document.querySelector("input[name=cargoMass]");
-        //   if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-        //       alert("All fields are required!");
-        //       // stop the form submission
-        //       event.preventDefault();
-        //   }
+        event.preventDefault();
+        //inputs
+        let pilotInput = document.querySelector("input[name=pilotName]");
+        let copilotInput = document.querySelector("input[name=copilotName]");
+        let fuelInput = document.querySelector("input[name=fuelLevel]");
+        let cargoInput = document.querySelector("input[name=cargoMass]");
+
+        //statuses
+        let pilotStatus = document.getElementById("pilotStatus");
+        pilotStatus.innerHTML = `Pilot ${pilotInput.value} is ready for launch.`
+        let copilotStatus = document.getElementById("copilotStatus");
+        copilotStatus.innerHTML = `Co-pilot ${copilotInput.value} is ready for launch.`
+        let fuelStatus = document.getElementById("fuelStatus");
+        let cargoStatus = document.getElementById("cargoStatus");
+        let launchStatus = document.getElementById("launchStatus")
+        let faultyItems = document.getElementById("faultyItems");
+
+        //conditonals for launch
+        if ((fuelInput.value >= 10000) && (cargoInput.value <= 10000)) {
+            faultyItems.style.visibility = "visible";
+            launchStatus.innerHTML = "SHUTTLE IS READY FOR LAUNCH!"
+            launchStatus.style.color = "#62fff4"
+        } else if ((fuelInput.value < 10000) || (cargoInput.value > 10000)) {
+            faultyItems.style.visibility = "visible";
+            launchStatus.innerHTML = "SHUTTLE IS NOT READY FOR LAUNCH!"
+            launchStatus.style.color = "#ff0163"
+        };
+
+        if (fuelInput.value < 10000) {
+            fuelStatus.innerHTML = "Fuel level is too low for launch!"
+        } else {
+            fuelStatus.innerHTML = "Fuel level high enough for launch."
+        };
+
+        if (cargoInput.value > 10000) {
+            cargoStatus.innerHTML = "Cargo mass is too great for launch!"
+        } else {
+            cargoStatus.innerHTML = "Cargo mass is low enough for launch."
+        };
+
     });
 });
 
